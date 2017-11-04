5a892714c92de6db451444f19595958f606697ef
Michael <michkna3@gmail.com>
2017-11-04 14:00:33 -0500

    created show route which will house weather dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,3 +2,19 @@ get '/users/new' do
   @user = User.new
   erb :'users/new'
 end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :'users/new'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'users/show'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ActiveRecord::Base
-  # has_many :locations
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
+  validate :password_length
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @plain_text_pass = new_password
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    if user && user.password == password
+      user
+    else
+      nil
+    end
+  end
+
+  def password_length
+    errors.add(:password, "must be at least 6 characters") if @plain_text_pass.length < 6
+  end
 end
