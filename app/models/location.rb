6b9dc3ff5f0df630b7db39403e9d568369c552c5
Michael <michkna3@gmail.com>
2017-11-04 16:15:39 -0500

    added gitignore file for keys

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,12 @@ class Location < ActiveRecord::Base
   belongs_to :user
 
   validates :address, presence: true
+
+  def coord_get
+    "https://maps.googleapis.com/maps/api/geocode/json?address=#{self.address}&key=YOUR_API_KEY"
+    # uri = URI.parse(URI.encode(endpoint))
+    # api_response = Net::HTTP.get(uri)
+    # response_collection = JSON.parse(api_response)
+    # pp(response_collection)
+  end
 end
