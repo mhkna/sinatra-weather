b9125ecd88b751f645d45b7cbc2c21ae632feeb6
Michael <michkna3@gmail.com>
2017-11-04 17:11:12 -0500

    google geocoding api is returning the coordinates when user enters an address

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,14 @@
 post '/locations' do
   @location = Location.new(params[:location])
   if @location.save
-    redirect "/users/#{current_user.id}"
+    redirect "/locations/#{@location.id}"
   else
     @errors = @location.errors.full_messages
     erb :'users/show'
   end
+end
+
+get '/locations/:id' do
+  @location = Location.find(params[:id])
+  erb :'locations/show'
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -3,11 +3,22 @@ class Location < ActiveRecord::Base
 
   validates :address, presence: true
 
-  def coord_get
-    "https://maps.googleapis.com/maps/api/geocode/json?address=#{self.address}&key=YOUR_API_KEY"
-    # uri = URI.parse(URI.encode(endpoint))
-    # api_response = Net::HTTP.get(uri)
-    # response_collection = JSON.parse(api_response)
-    # pp(response_collection)
+  def location_info_get
+    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{self.address}&key=#{ENV["GOOGLE_GEO_TOKEN"]}")
+    api_response = Net::HTTP.get(uri)
+    response_collection = JSON.parse(api_response)
+  end
+
+  def coords
+    coords_resp = location_info_get["results"][0]["geometry"]["location"]
+    lat = coords_resp["lat"]
+    lng = coords_resp["lng"]
+    "#{lat},#{lng}"
+  end
+
+  def weather_get
+    uri = URI.parse("https://api.darksky.net/forecast/#{ENV["WEATHER_TOKEN"]}/#{self.coords}")
+    api_response = NET::HTTP.get(uri)
+    response_collection = JSON.parse(api_response)
   end
 end
