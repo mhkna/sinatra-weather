191c7019d75ce90a4be766f078b8a50248b0a84d
Michael <michkna3@gmail.com>
2017-11-08 00:21:19 -0600

    added require to application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,6 @@
+//= require jsapi
+//= require chartkick
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
